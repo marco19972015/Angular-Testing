0a83a6e1e2dfed227761d1101a389b1eb7008add
Marco Torres <marcoantonio.torres@icloud.com>
2024-02-24 17:13:02 -0500

    In version 2, I focued on furthering the implementation of the first specification in the spec file. Then I used the toBe() utility function along with the expect function to tell Jasmin what is expected. I then added more description in case of a failure to the toBe() function utility by passing a second argument as a string and explain the reasoning of the failure.

--- src/app/courses/services/calculator.service.ts
+++ src/app/courses/services/calculator.service.ts
@@ -28,105 +28,60 @@ export class CalculatorService {
 
 
   // SIDE NOTES 
+  // We will be focusing on the implementations of the it('should add two numbers') specification
+  // The first thing we'll need is an instance of the CalculatorService service in the spec file
+  // we create it under the specification
+    // const calculator = new CalculatorService();
 
-  // WHAT IS JASMINE
-  // Jasmine is behavior drive JS framework, with Jasmine we have everything we need to test our code
-  // We have a way of both creating creating our test, we can run them, we can produce test reports 
-  // A complete batteries included solution
-
-  // In Jasmine our test are known as specification, or specs and they are group in test suites
-
-  // CREATING OUR FIRST TEST
-  // First we create a spec filed called calculator.service.spec.ts
-  // In order to define the test suite we use the Jasmine describe() function
-  // Inside the parameter we can give it a name, in this case we call it 'CalculatorService'
-  // So we give to the test suite the same name as the service we want to test
-    // describe('CalculatorService')
-
-  // Nest we pass in an in line function
-  // The function will contain a series of specifications
-  // A group of specifications is called a test suite 
-  // and a specification is simply a functional test
-
-    // describe('CalculatorService', () => {
-    
-    // })
-
-  // So a specification is meant to test a particular functional feature of the application
-
-  // We can now define our first specification.
-  // In order to define a specification we use the it() function
-
-  // Each specification should be described by a sentence starting with the word it
-  
-  // For example lets create a specification to test the add function
-  // the specification name for that could be it('should add two numbers')
-  // The goal being it should describe a functional feature of our application
-  // This should not be mean to test a functional aspect of our application
-  // It's really meant to test a functional feature
-
-    //   describe('CalculatorService', () => {
-    //     it('should add two numbers')
-    // })
-
-  // In our case, our Calculator service, it should be able to add two numbers
-
-  // The next argument is going to be a function 
-  // This function is going to contain the body of the specification
-
-    //   describe('CalculatorService', () => {
-    //     it('should add two numbers', () => {
-        
-    //     });
-    //   })
-  
-  // One test suite can contain multiple specifications
-
-  // for example we can add a second specification to test the second method in the service (the subtract method)
-    //   describe('CalculatorService', () => {
-    //     it('should add two numbers', () => {
-
-    //     });
-
-    //     it('should subtract two numbers', () => {
-
-    //     });
-    //   })
-
-  // NOTE WE HAVE NOT IMPLEMENTED THEM YET
-  // In order to tell Jasmine it's not yet ready to be executed we are going to use the
-  // pending() utility function on both test
-
+  // Now the calculator service is going to take a single dependency, which is the logger service
+  // The logger service itself does not have any dependencies, 
+  // so we can instatiate it inside the parameter of the CalculatorService instance by calling its contructor
     //   it('should add two numbers', () => {
-    //     pending();
-    //   });
-
-  // NOW we can run our inital test suite
-  // In the terminal we run 
-  // ng test
-  
-  // ng test will compile all of our application code as well as our specifications
-  // and it will run our test specification using the Karm test runner 
-  // Karma is used internally by the Angular CLI 
-  // ng test will also lanch an instance of the chrome browser where the test are going to be executed 
-  
-  // In the browser we can see 2 specs were found 
-  // They belong to CalculatorService test suite
-  // We have our two test specifications below that (we can see they are marked in yellow meaning they are pending)
-
-  // FEATURES OF THE KARMA TEST RUNNER ARE...
-  // Hot Reload Mode - Meaning if we change our test in calculator.service.spec.ts, the test report is going to get refreshed
-  // For example, we can simulate a failing test using the fail() utility function
-    //   it('should subtract two numbers', () => {
-    //     fail();
+    //     const calculator = new CalculatorService( new LoggerService);
     // });
-  // If we hit save, Karma will reconpile and we will have 1 failed test
-  // This is useful in order to fail a test in case a certain condition is reached in our code that should never be reached 
-  // if the code is working correctly 
+  // Now we have an instance of our CalculatorService
 
-  // If we don't want to use the Hot Reload mode, in the terminal we can run the command 
-  //  ng test --no-watch
+  // We can now use it in order to add two numbers 
+    // const result = calculator.add(2, 2);
+
+
+  // Now we need to ensure that this result is the expected result
+  // To do this we write first TEST ASSUR-SHANCE
+  // We use the expect() function utility from Jasmine as so
+    // expect();
+  // And we say we are going to expect() the result to be 4
+    // expect(result).toBe(4);
   
+  // Jasmine provides us multiple methods such as toBeCloseTo, toBeDefined, toBeGreaterThan, etc... to write 
+  // all types of ASSUR-SHANCE
+  
+  // This is our test ASSUR-SHANCE, this will make sure the test fails if it's not 4
 
+
+  // OUR FIRST SPECIFICATION LOOKS LIKE THIS
+    //   it('should add two numbers', () => {
+  // First, we have a setup phase, where we are preparing the components or services we want to test 
+    //     const calculator = new CalculatorService( new LoggerService());
+  // Second, we have an execution phase, where we are going to trigger the operation we want to test 
+    //     const result = calculator.add(2, 2);
+  // Lastly, we write a series of test ASSUR-SHANCE, that are either going to fail marking the test as fail
+  // or they are going to be successful, which marks the test as successful
+    //     expect(result).toBe(4);
+    // });
+  
+  // If we fail intentially (done in the subtraction service class)
+  // After the toBe(0, ), we can add a second argument that describes why the assurshance has failed such as 
+    // expect(result).toBe(0, "Unexpected subtraction result");
+  // The ability to add this description is available in all of Jasmin assurance utility methods 
+
+
+  // LETS SAY WE WANT TO TAKE THIS TEST ONE STEP FURTHER
+  // lets say we want to not only do we want to test the result of the calculation is correct
+  // BUT also we want to make sure that the logger service is only called once whenever we call this operation
+  
+  // Lets imagine that the LoggerSerive consumes expensive resources that should not be overused 
+  // So we'll make sure that it only gets called once per operation
+  // In order to test this we'll introduce the concept of Jasmine SPIE
+ 
 }
 
